06107db7b9941913124e3d4486639f1f2ef63f2e
Aga <agazgo.ml@gmail.com>
2024-08-25 18:48:49 +0100

    clean weather

---
+++ src/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+PARENT_DIR = Path(__file__).parent.resolve().parent
+DATA_DIR = PARENT_DIR / 'data'
+RAW_DATA_DIR = DATA_DIR / 'raw'
+PREPROCESSED_DATA_DIR = DATA_DIR / 'preprocessed'
